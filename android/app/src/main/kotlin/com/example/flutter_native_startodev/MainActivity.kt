48467bb630927060388e27103e6f661c6650cc25
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-01-17 22:15:55 -0300

    Created integration with kotlin code that add a floating button over other apps.

---
+++ android/app/src/main/kotlin/com/example/flutter_native_startodev/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.flutter_native_startodev
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.util.Log
+import android.widget.ImageView
+import com.yhao.floatwindow.FloatWindow
+import com.yhao.floatwindow.Screen
+
+class MainActivity: FlutterActivity() {
+    companion object {
+        const val CHANNEL = "floating_button"
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+
+            when(call.method) {
+                "create" -> {
+                    Log.d("APP_MESSAGE", "*** Method CREATE ****")
+                    val imageView = ImageView(applicationContext)
+                    imageView.setImageResource(R.drawable.plus)
+                    FloatWindow.with(applicationContext)
+                        .setView(imageView)
+                        .setWidth(Screen.width,.15F)
+                        .setHeight(Screen.width,.15F)
+                        .setX(Screen.width,.8F)
+                        .setY(Screen.height,.3F)
+                        .setDesktopShow(true)
+                        .build()
+
+                    //result.success("OK")
+                }
+                "show" -> {
+                    Log.d("APP_MESSAGE", "*** Method SHOW ****")
+                    FloatWindow.get().show()
+                }
+                "hide" -> FloatWindow.get().hide()
+                else -> {
+                    Log.d("APP_MESSAGE", "*** Method not implemented ****")
+                    //result.notImplemented()
+                }
+            }
+
+            /*
+            if(call.method == "getRandomNumber") {
+                val rand = Random.nextInt(100)
+                result.success(rand)
+            }
+            else {
+                result.notImplemented()
+            }*/
+        }
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  static const platform = const MethodChannel('floating_button');
+
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Floating Button Demo'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              '$count',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 50),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                platform.invokeMethod('create');
+              },
+              child: Text('Create'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                platform.invokeMethod('show');
+              },
+              child: Text('Show'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                platform.invokeMethod('hide');
+              },
+              child: Text('Hide'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                platform.invokeMethod('not_implemented_native_method');
+              },
+              child: Text('Not implemented'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
